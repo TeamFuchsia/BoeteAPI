aa955e7611f19933ea5da53e48609530c22230ea
Rense <rense@houwing.net>
2019-11-26 13:57:59 +0100

    nieuwe

---
+++ src/main/java/nl/fuchsia/configuration/WebConfig.java
@@ -0,0 +1,28 @@
+package nl.fuchsia.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "nl.fuchsia")
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").setCachePeriod(0);
+    }
+
+}

e74148b1b5a351f9263889c78c83a0739dbd6f9a
NiewoldR <51324102+NiewoldR@users.noreply.github.com>
2019-12-30 12:00:29 +0100

    Overgebleven teken na merge conflict verwijderd (#27)
    * Overgebleven teken na merge conflict verwijderd
    
    * Overgebleven teken na merge conflict verwijderd
    
    * Aangepaste test

--- src/test/java/nl/fuchsia/repository/ZaakRepositoryTest.java
+++ src/test/java/nl/fuchsia/repository/ZaakRepositoryTest.java
@@ -1,8 +1,6 @@
 package nl.fuchsia.repository;
 
 import nl.fuchsia.configuration.TestDatabaseConfig;
-import nl.fuchsia.model.Feit;
-import nl.fuchsia.model.Persoon;
 import nl.fuchsia.model.Zaak;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,8 +11,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,72 +19,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ZaakRepositoryTest {
 
-    @Autowired
-    private ZaakRepository zaakRepository;
+	@Autowired
+	private ZaakRepository zaakRepository;
 
-    @Autowired
-	private PersoonRepository persoonRepository;
-
-    @Autowired
-	private FeitRepository feitRepository;
-
-    @BeforeAll
+	@BeforeAll
 	public void setup() {
-		Persoon persoon = new Persoon(1, "Rense", "Houwing", "De buren", "10", "8402 GH", "Drachten", "123456789", LocalDate.of(1990, 10, 12));
-		persoonRepository.addPersoon(persoon);
 
-		Feit eersteFeit = new Feit("VBF-001", "Test", 500);
-		Feit tweedeFeit =  new Feit("VBF-002", "Test", 500);
-		feitRepository.addFeit(eersteFeit);
-		feitRepository.addFeit(tweedeFeit);
-		List<Feit> feitList = new ArrayList<>();
-		feitList.add(eersteFeit);
-		feitList.add(tweedeFeit);
-
-		Zaak zaak = new Zaak(LocalDate.of(2019, 12, 12), "Drachten",persoon,feitList);
-
+		Zaak zaak = new Zaak(LocalDate.of(2019, 12, 12), "Drachten");
 		zaakRepository.addZaak(zaak);
 	}
 
-    @Test
-    void addZaak() {
-		Zaak zaak = new Zaak(LocalDate.of(2019, 12, 12), "Leeuwarden",persoonRepository.getPersoonById(1),feitRepository.getFeiten());
-
+	@Test
+	void addZaak() {
+		Zaak zaak = new Zaak(LocalDate.of(2019, 12, 12), "Leeuwarden");
 		zaakRepository.addZaak(zaak);
 
-        assertThat(zaakRepository.getZaken()).hasSize(3);
+		assertThat(zaakRepository.getZaken()).hasSize(2);
 
-    }
+	}
 
-    @Test
-
-    void testGetZaakById() {
-		Zaak zaakByIdtest = new Zaak(LocalDate.of(2019, 12, 12), "Heerenveen",persoonRepository.getPersoonById(1),feitRepository.getFeiten());
-
+	@Test
+	void getZakenById() {
+		Zaak zaakByIdtest = new Zaak(LocalDate.of(2019, 12, 12), "Heerenveen");
 		zaakRepository.addZaak(zaakByIdtest);
 
-    	assertThat(zaakRepository.getZaakById(zaakByIdtest.getZaaknr()).getPleeglocatie()).isEqualTo("Heerenveen");
-    }
+		assertThat(zaakRepository.getZaakById(zaakByIdtest.getZaaknr()).getPleeglocatie()).isEqualTo("Heerenveen");
+	}
 
 	@Test
 	void getzaken() {
-		assertThat(zaakRepository.getZaken()).hasSize(4);
-	}
-
-	@Test
-	void getZakenByPersoon() {
-		Persoon persoon = persoonRepository.getPersoonById(1);
-
-		zaakRepository.getZakenByPersoon(persoon);
-
-		assertThat(zaakRepository.getZakenByPersoon(persoon)).hasSize(3);
-
-		List<Feit> feitList = new ArrayList<>();
-		feitList.add(feitRepository.getFeitById(2));
-		zaakRepository.addZaak(new Zaak(LocalDate.of(2020, 1, 1), "Groningen",persoon,feitList));
-
-		zaakRepository.getZakenByPersoon(persoon);
-
-		assertThat(zaakRepository.getZakenByPersoon(persoon)).hasSize(4);
+		assertThat(zaakRepository.getZaken()).hasSize(3);
 	}
 }

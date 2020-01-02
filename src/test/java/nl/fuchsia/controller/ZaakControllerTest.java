95231a6be12351582d70651899df4b58a4e5bafa
rniewold <r.niewold@cjib.nl>
2020-01-02 14:21:23 +0100

    Testen aangepast

--- src/test/java/nl/fuchsia/controller/ZaakControllerTest.java
+++ src/test/java/nl/fuchsia/controller/ZaakControllerTest.java
@@ -1,49 +1,49 @@
-//package nl.fuchsia.controller;
-//
-//import nl.fuchsia.dto.ZaakAddDto;
-//import nl.fuchsia.services.ZaakService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.MockitoAnnotations.initMocks;
-//
-//public class ZaakControllerTest {
-//
-//	@Mock
-//	ZaakService zaakService;
-//	@InjectMocks
-//	ZaakController zaakController;
-//
-//	@BeforeEach
-//	public void setup() {
-//		initMocks(this);
-//	}
-//
-//	@Test
-//	public void testGetZaken() {
-//		zaakController.getZaken();
-//
-//		verify(zaakService).getZaken();
-//	}
-//
-//    @Test
-//    public void testAddZaak() {
-//        ZaakAddDto zaakAddDto = new ZaakAddDto();
-//
-//        zaakController.addZaak(zaakAddDto);
-//
-//        verify(zaakService).addZaak(zaakAddDto);
-//    }
-//
-//	@Test
-//	void testGetZakenByPersoon() {
-//		int persoonnr = 1;
-//
-//		zaakController.getZakenByPersoon(persoonnr);
-//
-//		verify(zaakService).getZakenByPersoon(persoonnr);
-//	}
-//}
+package nl.fuchsia.controller;
+
+import nl.fuchsia.dto.ZaakAddDto;
+import nl.fuchsia.services.ZaakService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ZaakControllerTest {
+
+	@Mock
+	ZaakService zaakService;
+	@InjectMocks
+	ZaakController zaakController;
+
+	@BeforeEach
+	public void setup() {
+		initMocks(this);
+	}
+
+	@Test
+	public void testGetZaken() {
+		zaakController.getZaken(0);
+
+		verify(zaakService).getZaken();
+	}
+
+    @Test
+    public void testAddZaak() {
+        ZaakAddDto zaakAddDto = new ZaakAddDto();
+
+        zaakController.addZaak(zaakAddDto);
+
+        verify(zaakService).addZaak(zaakAddDto);
+    }
+
+	@Test
+	void testGetZakenByPersoon() {
+		int persoonnr = 1;
+
+		zaakController.getZaken(persoonnr);
+
+		verify(zaakService).getZakenByPersoon(persoonnr);
+	}
+}

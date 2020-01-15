42cfcd1bc8913eb27635513c5d244f180fc8a826
Sander ten Hoor <sthoor@quintor.nl>
2020-01-15 16:16:20 +0100

    ADD: Necessary bean added.

--- BoeteAPI-BackEnd/src/main/java/nl/fuchsia/configuration/WebConfig.java
+++ BoeteAPI-BackEnd/src/main/java/nl/fuchsia/configuration/WebConfig.java
@@ -1,8 +1,9 @@
 package nl.fuchsia.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -16,6 +17,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = "nl.fuchsia")
 //@PropertySource("classpath:application.properties")
 public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * Om één of andere reden is deze bean statisch nodig. Het is nog niet duidelijk waarom,
+     * maar zonder deze bean wordt @Value niet geëvalueerd.
+     *
+     * @return
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {

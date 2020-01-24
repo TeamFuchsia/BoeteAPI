71ac859b11ea2f87d6d88ff5995f42dc4696d3b8
rniewold <r.niewold1410@gmail.com>
2020-01-24 09:24:44 +0100

    web config verwijderd en properties aangepast

--- BoeteAPI-backend/src/main/java/nl/fuchsia/Application.java
+++ BoeteAPI-backend/src/main/java/nl/fuchsia/Application.java
@@ -2,10 +2,11 @@ package nl.fuchsia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 // Als parameter aan de JVM meegeven: -javaagent:D:/Work/.m2/repository/org/springframework/spring-instrument/5.2.3.RELEASE/spring-instrument-5.2.3.RELEASE.jar
 @SpringBootApplication
+@EnableWebMvc
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
